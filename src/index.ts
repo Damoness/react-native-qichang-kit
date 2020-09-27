3dfc04fa2c350b6b353c9eef6106f33d4c4dc1a8
Damoness <qjwdcool@qq.com>
2020-09-27 14:16:59 +0800

    feat: export component

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import Avatar from './Avatar';
+import Button from './Button';
+//import Image from './Image'
+import DropdownArrow from './DropdownArrow';
+import NavigationBackComponent from './NavigationBackComponent';
+import { Loading, LoadingComponent, ArticleLoading } from './Loading';
+// import {
+//   RefreshListView,
+//   RefreshState,
+//   PullDownRefreshAndPullUpLoadMoreListView,
+// } from '@damoness/react-native-refresh';
+
+export {
+  Avatar,
+  Button,
+  DropdownArrow,
+  //Image,
+  Loading,
+  LoadingComponent,
+  ArticleLoading,
+  NavigationBackComponent,
+  // RefreshListView,
+  // RefreshState,
+  // PullDownRefreshAndPullUpLoadMoreListView,
+};
