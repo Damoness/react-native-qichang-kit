53d22b11b3edcf176524f99ed01075afd263f55d
Damoness <qjwdcool@qq.com>
2020-09-27 15:12:06 +0800

    feat: change ThemeProvider

--- src/config/ThemeProvider.tsx
+++ src/config/ThemeProvider.tsx
@@ -4,15 +4,19 @@ import { useColorScheme } from 'react-native';
 import colors from './colors';
 import colorsDark from './colorsDark';
 
-export const ThemeContext = React.createContext({
+export const ThemeContext = React.createContext<{
+  colors: typeof colors;
+  theme: 'light' | 'dark';
+}>({
   colors: colors,
+  theme: 'light',
 });
 
 const ThemeProvider: React.FC = ({ children }) => {
-  let color = useColorScheme();
+  let color = useColorScheme() || 'light';
   const co = color === 'dark' ? colorsDark : colors;
   return (
-    <ThemeContext.Provider value={{ colors: co }}>
+    <ThemeContext.Provider value={{ colors: co, theme: color }}>
       {children}
     </ThemeContext.Provider>
   );
