c4fa8a27ac65f2664ca4b79fa5122683da1f7f92
Damoness <qjwdcool@qq.com>
2020-09-27 14:34:39 +0800

    fix: type error

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,12 @@
 import ThemeProvider from './ThemeProvider';
 import colors from './colors';
 import colorsDark from './colorsDark';
-import type theme from './theme';
 import withTheme from './withTheme';
 
-export type Theme = typeof theme;
+export type Theme = {
+  colors: typeof colors;
+};
+
 export type ThemeProps = {
   theme: Theme;
 };
