a038113fdd5cb8cfd9afb28beef0744f15f0d5b2
Damoness <qjwdcool@qq.com>
2021-03-12 19:49:53 +0800

    feat: 增加 NavigationBar 组件

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import Button from './Button';
 import Image from './Image';
 import DropdownArrow from './DropdownArrow';
 import NavigationBackComponent from './NavigationBackComponent';
+
 import { Loading, LoadingComponent, ArticleLoading } from './Loading';
 import ErrorComponent from './ErrorComponent';
 import ThemedLineView from './Themed/ThemedLineView';
 import ThemedText, { TextType as ThemedTextType } from './Themed/ThemedText';
-
+import NavigationBar from './NavigationBar';
 import Swiper from './Swiper';
 
 import {
@@ -38,6 +39,7 @@ export {
   LoadingComponent,
   ArticleLoading,
   NavigationBackComponent,
+  NavigationBar,
   ThemeProvider,
   ErrorComponent,
   DropdownArrow,
